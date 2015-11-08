e248f37ae4643773684002f2109857e02a41813f
ichigowa <mart657@newschool.edu>
2015-11-08 15:00:35 -0500

    Working health bar

--- Transparent/Assets/Scripts/PlayerHealth.cs
+++ Transparent/Assets/Scripts/PlayerHealth.cs
@@ -1,16 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour {
 	
 	
-	public float health;
-	
+	public int health = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+
+    public bool isDead;
+    public bool damaged;
+
+
+	 void Awake()
+    {
+        
+        currentHealth = health;
+    }
 	
 	void Start () {
 		
-		health = 20f;
-		
+	
 	
 	}
 	
@@ -20,11 +31,31 @@ public class PlayerHealth : MonoBehaviour {
 	}
 	
 	
-	void OnTriggerEnter(Collider other){
-		if (other.CompareTag("Glass"))
-		{
-			health -= 1f;
+	//void OnTriggerEnter(Collider other){
+	//	if (other.CompareTag("Glass"))
+	//	{
+	//		health -= 1;
 			
-		}
-	}
+	//	}
+	//}
+
+    public void TakeDamage(int ammount)
+    {
+        damaged = true;
+        currentHealth -= ammount;
+        healthSlider.value = currentHealth;
+
+        if(currentHealth <= 0 && isDead)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        // Set the death flag so this function won't be called again.
+        isDead = true;
+
+        Application.LoadLevel("gameOverScreen");
+    }
 }
