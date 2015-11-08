28eda43139c3f8e7b0a8542044a00298c1a15862
Orien Longo <longo788@newschool.edu>
2015-11-08 12:10:10 -0500

    Added Fonts

--- Transparent/Assets/Scripts/Destroy.cs
+++ Transparent/Assets/Scripts/Destroy.cs
@@ -1,42 +1,86 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Destroy : MonoBehaviour {
-	
-	bool mPressed;
-	// Use this for initialization
-	void Start () {
-		mPressed = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+public class Destroy : MonoBehaviour
+{
 
-		 if (Input.GetMouseButton(0)) {
-			mPressed = true;
-		}else{mPressed = false;}
-	
-	}
+    bool mPressed;
+    Collider beam;
+    public GameObject beamColor, beam1;
 
-	void OnTriggerEnter(Collider other){
-		
-			if (other.gameObject.CompareTag("Player") && mPressed ){
-			
-					
-				Destroy(other.gameObject);
-			}
-		
-	}
-	
-	void OnTriggerStay(Collider other){
-		
-			if (other.gameObject.CompareTag("Player")  && mPressed){
-			
-					
-				Destroy(other.gameObject);
-			}
-		
-	}
+    void Start()
+    {
+        //beamC = beamColor.GetComponent<Renderer>();
+        beam = beam1.GetComponent<Collider>();
+        beam.enabled = false;        
+        mPressed = false;
+    }
 
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            mPressed = true;
+            StartCoroutine(FadeTo(1.0f, .1f));
+            beam.enabled = true;
+            
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            beam.enabled = false;
+            mPressed = false;
+            StartCoroutine(FadeTo(0.0f, .1f));
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Player") && mPressed)
+        {
+
+            //gameObject.SetActive(true);
+
+            Destroy(other.gameObject);
+
+        }
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Player") && mPressed)
+        {
+
+
+            Destroy(other.gameObject);
+        }
+
+    }
+    IEnumerator FadeTo(float aValue, float aTime)
+    {
+        yield return new WaitForSeconds(.1f);
+        float alpha = beamColor.transform.GetComponent<Renderer>().material.color.a;
+
+        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / aTime)
+        {
+            Color newColor = beamColor.transform.GetComponent<Renderer>().material.color;
+            newColor.a = Mathf.Lerp(alpha, aValue, t);
+            beamColor.transform.GetComponent<Renderer>().material.color = newColor;
+            yield return null;
+
+            //if (beam.enabled == false)
+            //{
+            //    beam.enabled = true;
+            //} else
+            //{
+            //    beam.enabled = false;
+            //}
+        }
+    }
 
 }
